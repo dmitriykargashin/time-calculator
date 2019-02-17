82d6b0eac95bb8b4b63e58d520f9bef9c5ba792c
Dmitriy Kargashin <dmitriy.kargashin@gmail.com>
2019-02-17 14:06:34 -0500

    + add New Class for Token

--- app/src/main/java/com/dmitriykargashin/timecalculator/lexer/Token.kt
+++ app/src/main/java/com/dmitriykargashin/timecalculator/lexer/Token.kt
@@ -1,10 +1,13 @@
 /*
- * Copyright (c) 2019. Dmitrii Kargashin 2019
+ * Copyright (c) 2019. Dmitriy Kargashin
+ *
+ * Token class
  */
 
 package com.dmitriykargashin.timecalculator.lexer
 
 
+class Token(val type: TokenType, val strRepresentation: String = "", position: Int) {
 
 
-
+}
