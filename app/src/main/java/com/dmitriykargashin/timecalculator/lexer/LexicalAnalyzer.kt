9fdeca1c8008b1160e8018fb6701d9f0c1009822
Dmitriy Kargashin <dmitriy.kargashin@gmail.com>
2019-02-17 14:38:19 -0500

    + update class for Lexical Analyzer

--- app/src/main/java/com/dmitriykargashin/timecalculator/lexer/LexicalAnalyzer.kt
+++ app/src/main/java/com/dmitriykargashin/timecalculator/lexer/LexicalAnalyzer.kt
@@ -1,2 +1,23 @@
+/*
+ * Copyright (c) 2019. Dmitriy Kargashin
+ *
+ * Uses for Lexical analyze for string expression
+ */
+
 package com.dmitriykargashin.timecalculator.lexer
 
+class LexicalAnalyzer(var stringExrpession: String) {
+
+    // as result list of tokens
+    val listOfTokens: MutableList<Token> = ArrayList()
+    val currentPosition: Int = 0
+
+    fun analyze(): MutableList<Token> {
+        // removes spaces
+        stringExrpession = stringExrpession.replace(" ", "")
+
+        return listOfTokens
+    }
+
+
+}
