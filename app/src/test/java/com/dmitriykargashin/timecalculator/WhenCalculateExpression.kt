ee4392e83d81a47d3b42f620ee12274b67b6e617
Dmitriy Kargashin <dmitriy.kargashin@gmail.com>
2019-05-01 23:17:50 -0400

    + Improved Unit Tests - added Hamcrest and wrote customMatcher for comparing Tokens

--- app/src/test/java/com/dmitriykargashin/timecalculator/WhenCalculateExpression.kt
+++ app/src/test/java/com/dmitriykargashin/timecalculator/WhenCalculateExpression.kt
@@ -4,13 +4,44 @@
 
 package com.dmitriykargashin.timecalculator
 
+
 import com.dmitriykargashin.timecalculator.calculator.CalculatorOfTime
 import com.dmitriykargashin.timecalculator.extension.addStartAndEndSpace
 import com.dmitriykargashin.timecalculator.lexer.LexicalAnalyzer
+import com.dmitriykargashin.timecalculator.lexer.Token
 import com.dmitriykargashin.timecalculator.lexer.TokenType
 import com.dmitriykargashin.timecalculator.lexer.Tokens
+
+
+import org.hamcrest.Description
 import org.junit.Assert
 import org.junit.Test
+import org.hamcrest.MatcherAssert.assertThat
+
+import org.hamcrest.TypeSafeDiagnosingMatcher
+
+
+
+fun isEqualTo(expectedTokens: Tokens) = object : TypeSafeDiagnosingMatcher<Tokens>() {
+    override fun describeTo(description: Description) {
+        description.appendText("$expectedTokens")
+    }
+
+    override fun matchesSafely(tokens: Tokens, mismatchDescription: Description): Boolean {
+        var isMatches = true
+        if (tokens.size != expectedTokens.size) isMatches = false
+        else
+            for ((index, value) in tokens.withIndex()) {
+                if (!value.strRepresentation.equals(expectedTokens[index].strRepresentation)) {
+                    isMatches = false
+                }
+            }
+
+        mismatchDescription.appendText("$tokens ")
+        return isMatches
+    }
+}
+
 
 class WhenCalculateExpression {
     private fun CalculateExpression(stringExpression: String): Tokens {
@@ -58,11 +89,63 @@ class WhenCalculateExpression {
     fun Calculate_Expr_10Minute_plus_5Hour_Equals_5Hour10Minute() {
         val listOfResultTokens =
             CalculateExpression("10 ${TokenType.MINUTE.value} ${TokenType.PLUS.value.addStartAndEndSpace()}5 ${TokenType.HOUR.value}")
-
         Assert.assertEquals(3, listOfResultTokens.lastIndex)
         Assert.assertEquals("5", listOfResultTokens[0].strRepresentation)
         Assert.assertEquals(TokenType.HOUR.value, listOfResultTokens[1].type.value)
         Assert.assertEquals("10", listOfResultTokens[2].strRepresentation)
         Assert.assertEquals(TokenType.MINUTE.value, listOfResultTokens[3].type.value)
+    }
+
+    @Test
+    fun Calculate_Expr_10Minute_multiply_5_Equals_50Minute() {
+        val listOfResultTokens =
+            CalculateExpression("10 ${TokenType.MINUTE.value} ${TokenType.MULTIPLY.value.addStartAndEndSpace()}5")
+
+        Assert.assertEquals(1, listOfResultTokens.lastIndex)
+        Assert.assertEquals("50", listOfResultTokens[0].strRepresentation)
+        Assert.assertEquals(TokenType.MINUTE.value, listOfResultTokens[1].type.value)
+
+    }
+
+    @Test
+    fun Calculate_Expr_10Minute_plus_5Hour_Equals_5Hour10Minute_a() {
+        val listOfExpectedTokens = Tokens()
+
+        with(listOfExpectedTokens) {
+            add(Token(TokenType.NUMBER, "5", 0))
+            add(Token(TokenType.HOUR, 0))
+            add(Token(TokenType.NUMBER, "10", 0))
+            add(Token(TokenType.MINUTE, 0))
+        }
+
+
+        val stringExpression ="10 Minute + 5 Hour"
+           // "10 ${TokenType.MINUTE.value} ${TokenType.PLUS.value.addStartAndEndSpace()}5 ${TokenType.HOUR.value}"
+
+
+        val listOfResultTokens = CalculateExpression(stringExpression)
+
+        assertThat(listOfResultTokens, isEqualTo(listOfExpectedTokens))
+
+    }
+
+    @Test
+    fun Calculate_Expr_5Hour_Minus_10_Minute_Equals_4Hour50Minute() {
+        val listOfExpectedTokens = Tokens()
+
+        with(listOfExpectedTokens) {
+            add(Token(TokenType.NUMBER, "4", 0))
+            add(Token(TokenType.HOUR, 0))
+            add(Token(TokenType.NUMBER, "50", 0))
+            add(Token(TokenType.MINUTE, 0))
+        }
+
+
+        val stringExpression ="5 Hour ${TokenType.MINUS.value} 10 Minute"
+
+        val listOfResultTokens = CalculateExpression(stringExpression)
+
+        assertThat(listOfResultTokens, isEqualTo(listOfExpectedTokens))
+
     }
 }
